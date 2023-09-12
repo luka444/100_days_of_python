ca339b669a9eb248f7411bc3c73dbae6de985261
Luka <lukagoderdzishvili4@gmail.com>
2023-09-12 17:33:00 +0400

    Add Day 13 about debugging

---
+++ pythonProjects/Day 13/Debugging.py
@@ -0,0 +1,60 @@
+
+# -------------------
+# def my_function():
+#     for i in range(1,20):
+#         if i == 20:
+#             print('You got it!')
+
+# my_function()
+
+# def my_function():
+#     for i in range(1,21):
+#         if i == 20:
+#             print('You got it!')
+
+# my_function()
+# -------------------
+# import random
+# numbers = [1,2,3,4,5,6]
+# random_num = random.randint(1,6)
+# print(numbers[random_num])
+
+# import random
+# numbers = [1,2,3,4,5,6]
+# random_num = random.randint(0,5)
+# print(numbers[random_num])
+# --------------------
+# year = int(input("What's your year of birth? "))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year > 1994:
+#  print("You are a Gen Z.")
+
+# year = int(input("What's your year of birth? "))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year >= 1994:
+#  print("You are a Gen Z.")
+# ----------------------
+# age = input("How old are you?")
+# if age > 18:
+# print("You can drive at age {age}.")
+
+# age = int(input("How old are you?"))
+# if age > 18:
+#     print(f"You can drive at age {age}.")
+# ----------------------
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+# ----------------------
